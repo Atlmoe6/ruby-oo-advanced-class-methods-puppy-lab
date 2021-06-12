f1d71e85fb88e2d0edf8d1d1a1c70f84b3c6a108
Doe Mohamud <doe.mahamud1@gmail.com>
2021-06-12 16:46:22 -0400

    commit tree 65fb239bacbe8566d3b5e7d9a0bbe86ab385a9f6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+require 'pry'
+class Dog
+@@all = []
+
+attr_accessor :name
+def initialize(name)
+    @name = name
+    save
+
+end
+
+def self.all 
+    @@all
+end
+
+def self.clear_all
+    self.all.clear {|n|}
+end
+
+def self.print_all
+    puts self.all.map {|n| n.name}
+  
+end
+
+def save
+    @@all << self
+end
+end
